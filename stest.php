113fd47c8feec22caf53bf5f75e35591841bf082
frak <frak.off@gmail.com>
2012-08-25 13:12:40 +0100

    Refined test harness, lots of fail goodness

--- stest.php
+++ stest.php
@@ -4,6 +4,18 @@ ini_set('display_errors', 1);
 
 // $nonExistent = new Spi(5, 4, array());
 
-$settings = new Spi(0, 1, array('mode' => SPI_MODE_0));
-var_dump($settings);
+$spi = new Spi(0, 1, array(
+    'mode' => SPI_MODE_0,
+    'bits' => 4,
+    'speed' => 1000000,
+    'delay' => 1
+));
 
+$data = array(1, 2, 3, 4, 5, 6, 7, 8);
+$spi->write($data);
+
+$data = $spi->read();
+var_dump($data);
+
+$data = $spi->transfer($data);
+var_dump($data);
