a5729f81588162b3e1d42332229be0bfba27d20a
frak <frak.off@gmail.com>
2012-08-27 22:54:39 +0100

    Delay function prototype

--- php_spi.h
+++ php_spi.h
@@ -120,6 +120,15 @@ ZEND_END_ARG_INFO()
 #define Spi__getInfo_args NULL
 #endif
 
+PHP_METHOD(Spi, usecDelay);
+#if (PHP_MAJOR_VERSION >= 5)
+ZEND_BEGIN_ARG_INFO_EX(Spi__usecDelay_args, ZEND_SEND_BY_VAL, ZEND_RETURN_VALUE, 1)
+    ZEND_ARG_INFO(0, delay)
+ZEND_END_ARG_INFO()
+#else /* PHP 4.x */
+#define Spi__usecDelay_args NULL
+#endif
+
 #ifdef  __cplusplus
 } // extern "C"
 #endif
