a5575fc32a92fc04ca96656aeb2c27b1a1d77bca
frak <frak.off@gmail.com>
2012-09-04 16:39:54 +0100

    A simple raster test for an LED string

---
+++ examples/raster.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once 'classes/Image.php';
+
+$image = new Image('test.png');
+
+$spi   = new Spi(0, 0, array('speed' => 1000000));
+
+while(true) {
+    $spi->blockTransfer($image->getAll(), 1, true);
+    usleep(10000);
+}
